002ca1009fc2188ae376688a63cc78dc55e0a6f5
MesumAli2 <smesumali20@gmail.com>
2022-09-18 14:25:59 +0400

    Added expand more buttons to each item in the list

--- app/src/main/java/com/example/woof/MainActivity.kt
+++ app/src/main/java/com/example/woof/MainActivity.kt
@@ -1,40 +1,26 @@
-/*
- * Copyright (C) 2022 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+
 package com.example.woof
 
 import android.os.Bundle
+import android.widget.Space
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Card
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Scaffold
-import androidx.compose.material.Text
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ExpandLess
+import androidx.compose.material.icons.filled.ExpandMore
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -88,6 +74,7 @@ fun WoofApp() {
  */
 @Composable
 fun DogItem(dog: Dog, modifier: Modifier = Modifier) {
+    val expanded by remember{ mutableStateOf(false)}
 
     Card (modifier = modifier.padding(8.dp), elevation = 4.dp, shape = Shapes.medium){
 
@@ -98,6 +85,9 @@ fun DogItem(dog: Dog, modifier: Modifier = Modifier) {
         ) {
             DogIcon(dog.imageResourceId)
             DogInformation(dog.name, dog.age)
+            Spacer(modifier = modifier.weight(1f))
+            DogItemButton(expanded = expanded, onClick = { /*TODO*/ })
+            
 
         }
     }
@@ -160,10 +150,33 @@ fun DogInformation(@StringRes dogName: Int, dogAge: Int, modifier: Modifier = Mo
     }
 }
 
+
+@Composable
+private fun DogItemButton(
+    expanded : Boolean,
+    onClick : () -> Unit,
+    modifier : Modifier = Modifier
+){
+
+    IconButton(onClick = onClick ) {
+        Icon(imageVector = Icons.Filled.ExpandMore ,
+            tint = MaterialTheme.colors.secondary,
+            contentDescription = stringResource(id = R.string.expand_button_content_description))
+    }
+
+
+}
+
+
+
 @Composable
 fun WoofTopAppBar(modifier: Modifier = Modifier){
-    Row(modifier = modifier.fillMaxWidth().background(color = MaterialTheme.colors.primary), verticalAlignment = Alignment.CenterVertically) {
-        Image(modifier = Modifier.size(64.dp).padding(8.dp),painter = painterResource(id = R.drawable.ic_woof_logo), contentDescription = null )
+    Row(modifier = modifier
+        .fillMaxWidth()
+        .background(color = MaterialTheme.colors.primary), verticalAlignment = Alignment.CenterVertically) {
+        Image(modifier = Modifier
+            .size(64.dp)
+            .padding(8.dp),painter = painterResource(id = R.drawable.ic_woof_logo), contentDescription = null )
         Text(text = stringResource(id = R.string.app_name),
         style = MaterialTheme.typography.h1)
 
